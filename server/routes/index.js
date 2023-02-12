c1d81c85ad8bb542c34361f6a7d13a3a18313fcd
Krystal <lopezkrystal10@gmail.com>
2023-02-12 15:37:11 -0800

    updated syntax on login function

--- server/routes/index.js
+++ server/routes/index.js
@@ -11,7 +11,6 @@ const { JWT_SECRET } = process.env;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-//TODO: Complete login and register functions, make sure that passwords are hashed and make sure all modules are installed
 
 /* Once authorization/authentication middleware is created, we need to make we add that to each route, so that each CRUD route is only accessible once the user has logged in */
 
